ce28089c1ad24638c98ddd8559a2c125573f42fe
Jagadeesh712 <60870224+Jagadeesh712@users.noreply.github.com>
2020-02-28 01:39:34 -0800

    Add files via upload

--- LCM.py
+++ LCM.py
@@ -1,14 +1,14 @@
-x = int(input("enter num"))
-y= int(input("enter num"))
-def computer_lcm(x,y):
-    if (x>y):
+num1 = int(input("enter value for num1:"))
+num2 = int(input("enter value for num2:"))
+print("the lcm is ", compute_lcm(num1, num2))
+def compute_lcm(x,y):
+    if(x > y):
         greater = x
     else:
-        greater=y
-    while true:
-        if (greater % x==0)and (greater % y==0):
-            lcm = greater
-            break
-            greater +=1
-            return lcm
-        print("lcm of two numbers is ".lcm)
+        greater = y
+        while true:
+            if(greater % x ==0) and (greater % y ==0):
+                lcm = greater
+                break;
+                greater += 1
+        return lcm
